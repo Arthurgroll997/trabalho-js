1282870bc4f6d3289129fc78e60bfc2fa66bf560
Arthrugroll123 <apvgroll@gmail.com>
2022-11-24 22:54:35 -0300

    Início da implementação

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # trabalho-js
 Trabalho de JavaScript do terceiro trimestre do curso de Desenvolvimento de Sistemas do IFRS Campus Canoas
+
+### Autores: Arthur Von Groll e Milena Schmitt Scheidt
--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello, World!");
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(3000, () => console.log("Ouvindo..."));
